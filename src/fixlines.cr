b56401305f6bce9a8f6c55667b0b64073c48f74d
Marc Chantreux <eiro@phear.org>
2020-06-26 21:47:28 +0200

    add last "\n"
    not sure it's the right thing to do but acceptable behaviour

--- src/fixlines.cr
+++ src/fixlines.cr
@@ -2,11 +2,7 @@
 VERSION = "0.1.0"
 seen = {} of String => Hash(Int32,String)
 
-ARGF
-.gets_to_end
-.split("\n")
-.tap( &.pop )
-.map { |change|
+ARGF.each_line { |change|
     path,num,content = change.split(':',3)
     ( seen[path] ||= {} of Int32 => String )[num.to_i] = content
 }
@@ -14,5 +10,5 @@ ARGF
 seen.each { |path,changes|
     lines = File.read_lines path
     changes.each { |no,content| lines[no-1] = content }
-    lines = File.write(path, lines.join "\n")
+    lines = File.write(path, (lines.join "\n")+"\n")
 }
